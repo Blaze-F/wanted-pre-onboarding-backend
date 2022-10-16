603bf34a448385b164c4d90fdb3f033eff5861ee
JunH <101803254+Blaze-F@users.noreply.github.com>
2022-10-17 00:05:19 +0900

    update model

--- Django/preonboard/forms.py
+++ Django/preonboard/forms.py
@@ -5,10 +5,14 @@ from preonboard.models import Job_opening
 class JobOpeningForm(forms.ModelForm):
     class Meta:
         model = Job_opening  # 사용할 모델
-        fields = ['title', 'company', 'content', 'stack', 'reward']  #모델의 속성
+        fields = ['title', 'company', 'country', 'location', 'content', 'stack', 'wanted', 'reward']  #모델의 속성
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'country': forms.TextInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'wanted': forms.TextInput(attrs={'class': 'form-control'}),
             'company': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 8}),
-            'stack': forms.Textarea(attrs={'class': 'form-control', 'rows': 8}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'stack': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'reward': forms.TextInput(attrs={'class': 'form-control'}),
         }
--- Django/preonboard/models.py
+++ Django/preonboard/models.py
@@ -7,6 +7,11 @@ class Job_opening(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     company = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    wanted = models.CharField(max_length=200)
+
+
     reward = models.IntegerField()
     content = models.TextField()
     stack = models.TextField()
